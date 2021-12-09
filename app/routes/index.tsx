1bf99d851d8482c5e4e08ad5d09424daf9b0d291
Travers <jtpinkerton@gmail.com>
2021-12-09 10:16:24 -0600

    second

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,100 +1,108 @@
 import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import { useLoaderData, json } from "remix";
+import { BudgetData, getBudgetsForMonth } from "../lib/lm-api";
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
+// <Head>
+//   <title>Budget</title>
+//   <link
+//     rel="apple-touch-icon"
+//     sizes="128x128"
+//     href="/apple-touch-icon.png"
+//   ></link>
+// </Head>;
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
+// export async function getServerSideProps({ req, params }) {
+//   if (req.cookies.lm_secret !== process.env.SECRET) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  // https://remix.run/api/remix#json
+//   const todaysMonth = new Date().getMonth();
+//   const { budgets, total } = await getBudgetsForMonth();
+
+//   return {
+//     props: {
+//       budgets,
+//       total,
+//     },
+//   };
+// }
+
+export const loader: LoaderFunction = async () => {
+  let data = await getBudgetsForMonth();
   return json(data);
 };
 
-// https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
+    title: "Budget",
   };
 };
 
-// https://remix.run/guides/routing#index-routes
-export default function Index() {
-  let data = useLoaderData<IndexData>();
-
+export default function Budget() {
+  let { budgets, total } = useLoaderData<BudgetData>();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
-        <h2>Demos In This App</h2>
+    <>
+      <form>
+        <select name="month" className="rounded w-full mb-4">
+          {months.map((month, index) => (
+            <option
+              key={month}
+              label={month}
+              value={index}
+              selected={index === new Date().getMonth()}
+            />
+          ))}
+        </select>
+      </form>
+      {!budgets.length ? (
+        <p>hmm something is wrong</p>
+      ) : (
         <ul>
-          {data.demos.map(demo => (
-            <li key={demo.to} className="remix__page__resource">
-              <Link to={demo.to} prefetch="intent">
-                {demo.name}
-              </Link>
+          {budgets.map((budget, index) => (
+            <li
+              className="mb-2 p-2 rounded shadow-sm bg-yellow-100 flex justify-between"
+              key={`${budget.category_id}_${String(index)}`}
+            >
+              {budget.category_name}:{" "}
+              <span
+                className={
+                  Number(budget.amount.slice(1)) > 0
+                    ? "text-green-500"
+                    : "text-red-500"
+                }
+              >
+                {budget.amount}
+              </span>
             </li>
           ))}
         </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map(resource => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
-    </div>
+      )}
+      {/* <p>Budgeted: {total.budgeted}</p>
+      <p>Spent: {total.spent}</p>
+      <p>
+        {total.budgeted > total.spent
+          ? `${total.spent - total.budgeted} over budget`
+          : `${total.budgeted - total.spent} under budget`}
+      </p> */}
+    </>
   );
 }
